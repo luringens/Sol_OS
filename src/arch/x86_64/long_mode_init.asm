8a56e54e0f2084516b07c06e731cf00b6e3fb173
Stian Soltvedt <soltvedt.stian@gmail.com>
2017-04-12 19:39:03 +0200

    Add rust hello world

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -11,7 +11,16 @@ long_mode_start:
     mov fs, ax
     mov gs, ax
 	
-    ; print `OKAY` to screen
-    mov rax, 0x2f592f412f4b2f4f
-    mov qword [0xb8000], rax
+    ; call the rust main
+    extern rust_main
+    call rust_main
+
+.os_returned:
+    ; rust main returned, print `OS returned!`
+    mov rax, 0x4f724f204f534f4f
+    mov [0xb8000], rax
+    mov rax, 0x4f724f754f744f65
+    mov [0xb8008], rax
+    mov rax, 0x4f214f644f654f6e
+    mov [0xb8010], rax
     hlt
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+#![feature(const_fn)]
+#![feature(lang_items)]
+#![feature(unique)]
+#![no_std]
+
+extern crate rlibc;
+extern crate volatile;
+extern crate spin;
+
+#[macro_use]
+mod vga_buffer;
+
+#[no_mangle]
+pub extern fn rust_main() {
+    vga_buffer::clear_screen();
+    println!("Hello World{}", "!");
+
+    loop{}
+}
+
+#[lang = "eh_personality"]
+extern fn eh_personality() {}
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern fn panic_fmt() -> ! {loop{}}
+
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn _Unwind_Resume() -> ! {loop {}}
